79a8ab30071e24e71c94c0aec6a9d5a86984b877
Rex Lo <LORE2@OOCL.COM>
2018-12-06 13:57:15 +0800

    added router and route and link

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => (
+  <div>
+    <Router>
+      <div>
+        <h2>ABOUT</h2>
+        <nav>
+          <Link to="/about">about</Link>
+        </nav>
+        <div>
+          <Route path="/about" />
+        </div>
+
       </div>
-    );
-  }
-}
+    </Router>
+  </div>
+);
 
 export default App;
