aca372599ea8bf0cc253d44ec2beb4388eb1a1b2
Rex Lo <LORE2@OOCL.COM>
2018-12-06 14:13:42 +0800

    added users home about page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { Switch, Link, Route } from "react-router-dom";
 import './App.css';
+import AboutPage from './components/AboutPage'
+import NotFoundPage from './components/NotFoundPage'
 
-const App = () => (
-  <div>
-    <Router>
-      <div>
-        <h2>ABOUT</h2>
+class App extends Component{
+  render(){
+    return(
+    <div>
         <nav>
-          <Link to="/about">about</Link>
+          <ul>
+            <li>
+              <Link to="/about">about</Link>
+            </li>
+            <li>
+              <Link to="/users">users</Link>
+            </li>
+            <li>
+              <Link to="/">home</Link>
+            </li>
+          </ul>
         </nav>
-        <div>
-          <Route path="/about" />
-        </div>
-
-      </div>
-    </Router>
-  </div>
-);
+        <hr/>
+        <Switch>
+          <Route path="/" exact component = {(<h1>home page</h1>)}/>
+          <Route path="/about" exact component = {AboutPage}/>
+          <Route path="/users" exact component = {(<h1>users page</h1>)}/>
+          <Route path="/news" exact component = {(<h1>news page</h1>)}/>
+          <Route component = {NotFoundPage}/>
+        </Switch>
+    </div>
+    )
+  }
+}
 
 export default App;
