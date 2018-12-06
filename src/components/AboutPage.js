53b6f1ecd9c4912324cb69fc50bc4a0205cd7232
Rex Lo <LORE2@OOCL.COM>
2018-12-06 14:44:48 +0800

    feat: go back button

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export default class AboutPage extends Component {
+    onClick =() =>{
+        const {history} = this.props
+        //history.goBack()
+        history.goForward()
+    }
+
   render() {
     return (
       <div>
         AboutPage
+        <button onClick = {this.onClick}>
+            go back
+        </button>
       </div>
     )
   }
